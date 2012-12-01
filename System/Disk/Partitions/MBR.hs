fa2702240da9ff7bde5451bc05bbac66e4c48538
startling <tdixon51793@gmail.com>
2012-12-01 00:32:54 -0500

    BootRecord now can be a Binary instance.

--- System/Disk/Partitions/MBR.hs
+++ System/Disk/Partitions/MBR.hs
@@ -80,9 +80,10 @@ data BootRecord = BootRecord
   , bootSig    :: Word16 }
   deriving (Eq, Show)
 
--- | Parse out a master boot record.
-getBootRecord :: Get BootRecord
-getBootRecord = BootRecord <$> getByteString 446 <*> get <*> getWord16le
+instance Binary BootRecord where
+  get = BootRecord <$> getByteString 446 <*> get <*> getWord16le
+  put = (sequence_ .) . sequence $ [ put . bootloader
+    , put . partitions , putWord16le . bootSig ]
 
 -- | Get the completely-optional, obsolete disk timestamp used by some old
 -- versions of Windows.
