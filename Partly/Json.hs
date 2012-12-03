30dc83fb46b56f5c835b838255e268935c2b4462
startling <tdixon51793@gmail.com>
2012-12-02 22:50:28 -0500

    a module with ToJSON instances for those types.

---
+++ Partly/Json.hs
@@ -0,0 +1,40 @@
+{-# Language OverloadedStrings #-}
+module Partly.Json where
+-- base:
+import Text.Printf
+-- aeson:
+import Data.Aeson
+-- partly:
+import System.Disk.Partitions.MBR
+
+instance ToJSON CHS where
+  toJSON (CHS h s c) = object
+    [ "cylinder" .= c
+    , "head" .= h
+    , "sector" .= s ]
+
+instance ToJSON PartitionEntry where
+  toJSON p = object
+    [ "chsFirst" .= chsFirst p
+    , "lbaFirst" .= lbaFirst p
+    , "chsLast" .= chsLast p
+    , "sectorCount" .= sectors p
+    -- TODO: give this some additional structure, e.g.
+    -- { asNum : ... , bootable : ... }
+    , "status" .= status p
+    -- TODO: give this some additional structure, e.g.
+    -- { asNum : ..., type : ... }
+    , "partitionType" .= partitionType p ]
+    -- TODO: partition size, etc
+
+instance ToJSON PartitionTable where
+  toJSON (PartitionTable _1 _2 _3 _4) = toJSON [_1, _2, _3, _4]
+
+instance ToJSON BootRecord where
+  toJSON b = object
+    [ "bootSignature" .= object
+      [ "asNum" .= bootSig b
+      , "asHex" .= (printf "0x%04x" :: String) ]
+    , "partitions" .= partitions b
+    -- TODO: should I include this here? idk.
+    , "bootloader" .= bootloader b ]
