d348b6030ce7d191b7dfb125f37d6f037a659ef3
startling <tdixon51793@gmail.com>
2012-12-04 01:11:41 -0500

    a test suite for the JSON stuff.

---
+++ test-json.hs
@@ -0,0 +1,42 @@
+module Main where
+-- base
+import Control.Monad
+import Control.Applicative
+import System.Exit
+-- bytestring
+import qualified Data.ByteString.Lazy as L
+-- aeson
+import Data.Aeson (Value, decode, FromJSON, encode)
+-- QuickCheck
+import Test.QuickCheck
+-- partly
+import System.Disk.Partitions.MBR
+import System.Disk.Partitions.MBR.Gen
+import Partly.Json
+
+-- | Test that we can serialize and deserialize a thing
+-- to and from JSON without losing information.
+iso :: (Eq a, FromJSON a) => (a -> L.ByteString) -> a -> Bool
+iso e c = decode (e c) == Just c
+
+testIso :: (Eq a, Show a, FromJSON a, Arbitrary a)
+  => String -> (a -> L.ByteString) -> IO Bool
+testIso s e = do
+  putStrLn $ "--> " ++ s
+  r <- quickCheckResult $ iso e
+  return True
+
+resultToBool :: Result -> Bool
+resultToBool (Success _ _ _) = True
+resultToBool (GaveUp _ _ _) = False
+resultToBool (Failure _ _ _ _ _ _ _) = False
+
+main :: IO ()
+main = do
+  putStrLn "Testing whether 'encode' and 'fromJust . decode' form an isomorphism."
+  is <- all id <$> sequence
+    [ testIso "CHS" (encode :: CHS -> L.ByteString)
+    , testIso "PartitionEntry" (encode :: PartitionEntry -> L.ByteString)
+    , testIso "PartitionTable" (encode :: PartitionTable -> L.ByteString)
+    , testIso "BootRecord" (encode :: BootRecord -> L.ByteString) ]
+  unless is exitFailure
