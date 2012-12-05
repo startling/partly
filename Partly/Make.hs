51982d1e639dc01324c36a954bdc430c780b8b88
startling <tdixon51793@gmail.com>
2012-12-04 21:23:50 -0500

    Set the signature of an MBR.
    also reorganized things to use "Delta".

--- Partly/Make.hs
+++ Partly/Make.hs
@@ -1,6 +1,7 @@
 module Partly.Make where
 -- base:
 import Data.List
+import Data.Char
 import Data.Word
 import Control.Applicative
 -- bytestring:
@@ -23,10 +24,15 @@ import Partly.Json
 
 data MakeOptions = MakeOptions
   { from   :: Maybe FilePath
-  , btl    :: Maybe FilePath
   , json   :: Bool
   , ugly   :: Bool
-  , output :: Maybe FilePath }
+  , output :: Maybe FilePath
+  , change :: Delta }
+  deriving (Eq, Show)
+
+data Delta = Delta
+  { btl :: Maybe FilePath
+  , sig :: Maybe Word16 }
   deriving (Eq, Show)
 
 makeOptions :: Parser MakeOptions
@@ -36,11 +42,6 @@ makeOptions = MakeOptions
     & short 'f'
     & metavar "file"
     & help "A bootloader to base this one on, either binary or JSON." )
-  <*> maybeOption
-    ( long "bootloader"
-    & short 'b'
-    & metavar "file"
-    & help "A file to include as a bootloader." )
   <*> switch
     ( long "json"
     & short 'j'
@@ -56,14 +57,38 @@ makeOptions = MakeOptions
     & help "Write to a file rather than stdout." )
   -- TODO: should "output" be required, so that "partly make" gives you
   -- a help screen?
+  <*> (Delta
+    <$> maybeOption
+      ( long "bootloader"
+      & short 'b'
+      & metavar "file"
+      & help "A file to include as a bootloader." )
+    <*> (fmap . (=<<)) getSignature (maybeOption
+      ( long "signature"
+      & short 's'
+      & metavar "sig"
+      & help "Set the boot signature to some uint16; also accepts 't' and 'f'.")))
   where
     maybeOption :: Mod OptionFields (Maybe String) -> Parser (Maybe String)
     maybeOption m = nullOption $ reader (Just . Just) & m & value Nothing
+    getSignature :: String -> Maybe Word16
+    getSignature s = case toLower <$> s of
+      "t" -> Just 0xaa55; "true" -> Just 0xaa55;
+      "f" -> Just 0x0000; "false" -> Just 0x0000;
+      _ -> case reads s of [(v, "")] -> Just v; _ -> Nothing;
 
 makeParser :: ParserInfo MakeOptions
 makeParser = info makeOptions
   ( progDesc "Create an MBR, potentially based on some existing one."
   & fullDesc)
+
+applyDelta :: Delta -> BootRecord -> IO BootRecord
+applyDelta d b = do
+  b <- apply b (btl d) $ \p -> (`fmap` B.readFile p)
+    $ \x -> b { bootloader = x}
+  b <- apply b (sig d) $ \x -> return b { bootSig = x }
+  return b
+  where apply b v fn = maybe (return b) fn v
 
 make :: MakeOptions -> IO ()
 make m = do
@@ -72,15 +97,12 @@ make m = do
     (Just f) -> case ".json" `elem` tails f of
        True -> L.readFile f >>= decodeJson
        False -> fmap (runGet get) $ L.readFile f
-  new <- case btl m of
-    Nothing -> return base
-    Just n -> (`fmap` B.readFile n) $ \x -> base { bootloader = x }
-  let l = getter new
-  maybe (L.putStr l) (flip L.writeFile l) $ output m
+  new <- writer <$> applyDelta (change m) base
+  maybe (L.putStr new) (flip L.writeFile new) $ output m
   return ()
   where
     decodeJson = maybe (fail "problems reading JSON") return . decode
-    getter = case (json m, ugly m) of
+    writer = case (json m, ugly m) of
       (False, _) -> runPut . put
       (True, True) -> encode
       (True, False) -> encodePretty
