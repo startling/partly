38590d6a117e7f398a33017e89ebc98c47fbaf29
startling <tdixon51793@gmail.com>
2012-12-05 04:23:29 -0500

    reorganized things.

--- Partly/View.hs
+++ Partly/View.hs
@@ -20,9 +20,9 @@ viewJson :: (Input, Display) -> IO ()
 viewJson (i, d) = input i >>= display d
 
 -- | We can turn a labelled field of a boot record into a command.
-fieldCommand :: Outputs b => (BootRecord -> b)
-  -> String -> InfoMod ViewCommand -> Mod CommandFields ViewCommand
-fieldCommand fn s m = command s . flip info m $
+fieldCommand :: Outputs b => String -> (BootRecord -> b)
+  -> InfoMod ViewCommand -> Mod CommandFields ViewCommand
+fieldCommand s fn m = command s . flip info m $
   ViewField 
     <$> parseInput "The file to parse and inspect."
     <*> ((. fn) <$> output <$> parseOutput)
@@ -37,15 +37,13 @@ viewParser = info
     ( command "json"
       ( info (ViewJson <$> viewJsonOptions )
         ( progDesc "Read a boot record into JSON." ))
-    & fieldCommand sigFn "signature"
+    & fieldCommand "signature"
+      ( printf "%04x" . bootSig :: BootRecord -> String )
       ( progDesc "View the boot signature of an MBR." )
-    & fieldCommand bootloader "bootloader"
+    & fieldCommand "bootloader" bootloader
       ( progDesc "View the bootloader of an MBR." )))
   ( progDesc "Inspect a boot record."
   & fullDesc )
-  where
-    sigFn :: BootRecord -> String
-    sigFn = printf "%04x" . bootSig
 
 view :: ViewCommand -> IO ()
 view c = case c of
